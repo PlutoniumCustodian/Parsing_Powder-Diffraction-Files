af39b689ffcb1456cb77478974ce6d34622476d6
PlutoniumCustodian <Titus@psu.edu>
2022-04-04 16:22:34 -0400

    fixes bug with the handeling of empty cells

--- XRD_PDF_Parse.py
+++ XRD_PDF_Parse.py
@@ -13,10 +13,11 @@ print(pd.DataFrame(f_name, columns=['File Name']))
 #Reads each sheet from the files into dictionary with each sheet becomes a df
 # for x in f_howmany:
 #     df = pd.read_excel(os.path.join('File_To_Parse', f_name[x]), header=None, usecols='A',nrows=27, sheet_name=None)
-    
+nanVer = ['#N/A', '#N/A N/A', '#NA', '-1.#IND', '-1.#QNAN', '-NaN', '-nan', 
+          '1.#IND', '1.#QNAN', '<NA>', 'N/A', 'NA', 'NULL', 'NaN', 'n/a', 'nan', 'null', ' ']
 x = int(input("Enter index of file to parse")) # select index of file you want from 'f_name' list
 SheetDict = pd.read_excel(os.path.join('File_To_Parse', f_name[x]), header=None,
-                   usecols='A',nrows=27, sheet_name=None, dtype=str)
+                   usecols='A',nrows=27, sheet_name=None, dtype=str, skip_blank_lines=False, na_values=nanVer, keep_default_na=False)
 
 # get list of keys in SheetDict
 key_list = []
